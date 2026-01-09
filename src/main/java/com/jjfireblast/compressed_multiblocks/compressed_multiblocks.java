36f122069ec1cfa27b5cad9fee310b73b9f0c141
jjfireblast101 <jjfireblast@gmail.com>
2026-01-09 01:22:41 +0000

    More Initial Setup

--- src/main/java/com/jjfireblast/compressed_multiblocks/compressed_multiblocks.java
+++ src/main/java/com/jjfireblast/compressed_multiblocks/compressed_multiblocks.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.jjfireblast.compressed_multiblocks;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -29,8 +29,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(compressed_multiblocks.MODID)
+public class compressed_multiblocks
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -60,7 +60,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public compressed_multiblocks(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
